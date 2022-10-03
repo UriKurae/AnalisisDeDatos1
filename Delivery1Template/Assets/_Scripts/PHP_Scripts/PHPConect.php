e7f715bef808817e9023a15fb63a8f9fbab51944
Alejandro Moreno <61042882+JandroK@users.noreply.github.com>
2022-10-03 14:15:23 +0200

    Insert and show dates

--- Delivery1Template/Assets/_Scripts/PHP_Scripts/PHPConect.php
+++ Delivery1Template/Assets/_Scripts/PHP_Scripts/PHPConect.php
@@ -11,4 +11,19 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+$sql = "INSERT INTO `Users` (`IdUser`, `Name`, `Country`, `Date`) VALUES ('265254215', 'testUser12', 'Suiza', '2022-5-29')";
+$result = $conn->query($sql);
+
+//$sql = "SELECT IdUser, Name, Country, Date FROM Users";
+
+// if ($result->num_rows > 0) {
+//   // output data of each row
+//   while($row = $result->fetch_assoc()) {
+//     echo "IdUser: " . $row["IdUser"]. " - Name: " . $row["Name"]. "Country: " . $row["Country"]. "Date: " . $row["Date"]. "<br>";
+//   }
+// } else {
+//   echo "0 results";
+// }
+$conn->close();
 ?>
